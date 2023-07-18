59f024a1c98e4d4a4f581ebe22eda5c5065e82ff
Egor Kirin <egorcom27@gmail.com>
2023-07-18 22:59:51 +0300

    воспроизведение звуков и регулировка громкости у каждого звука

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const sliders = document.querySelectorAll(".sliderAmbient");
+const playBtn = document.querySelector("#playMainBtn");
+const playBtnIcon = document.querySelector(
+  "#playMainBtn .play, #playMainBtn .pause"
+);
 
 const musicMap = {
   "slider-1": "assets/music/glue-rain.mp4",
@@ -6,18 +10,46 @@ const musicMap = {
   "slider-3": "assets/music/main-fire.mp4",
 };
 
+let soundMusicElements = [];
+
+function playMusic() {
+  if (playBtnIcon.classList.contains("pause")) {
+    playBtnIcon.classList.remove("pause");
+    playBtnIcon.classList.add("play");
+    soundMusicElements.forEach((audio) => audio.pause());
+  } else {
+    playBtnIcon.classList.remove("play");
+    playBtnIcon.classList.add("pause");
+    soundMusicElements.forEach((audio) => audio.play());
+  }
+}
+
 function updateMusic(e) {
   const sliderId = e.target.id;
   const sliderValue = parseFloat(e.target.value);
 
-  const musicContainer = document.querySelector(`#${sliderId}`).closest(".sound__item").querySelector(".sound__music");
+  const musicContainer = document
+    .querySelector(`#${sliderId}`)
+    .closest(".sound__item")
+    .querySelector(".sound__music");
 
-  if (sliderValue !== 0) {
+  let audioElement = musicContainer.querySelector("audio");
+
+  if (!audioElement) {
     const audioHTML = `<audio preload="auto" src="${musicMap[sliderId]}" loop></audio>`;
     musicContainer.innerHTML = audioHTML;
+    audioElement = musicContainer.querySelector("audio");
+    soundMusicElements.push(audioElement);
+  }
+  audioElement.volume = sliderValue;
+
+  if (playBtnIcon.classList.contains("pause")) {
+    audioElement.play();
   }
 }
 
 sliders.forEach((slider) => {
   slider.addEventListener("input", updateMusic);
 });
+
+playBtn.addEventListener("click", playMusic);
